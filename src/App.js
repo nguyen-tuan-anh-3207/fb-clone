b2a90add835b166cf05e9edeb9e0513687479243
Tuan Anh <tuananhnguyen3207@gmail.com>
2021-08-05 21:16:36 +0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import FrontEndRoutes from './areas'
 import HomePage from './features/home'
 import './scss/index.scss'
 
 function App() {
   return (
-    <>
-      <HomePage />
-      {/* <Router>{FrontEndRoutes()}</Router> */}
-    </>
+    <Router>
+      <Switch>
+        <FrontEndRoutes />
+      </Switch>
+    </Router>
   )
 }
 
--- src/routes/mainNav.js
+++ src/routes/mainNav.js
@@ -1,5 +1,9 @@
 import { homeNav, homeRoutes } from '../features/home/nav'
 
-export const mainNav = [homeNav]
+export const mainNav = [...homeNav]
+
+console.log('mainNav', mainNav)
 
 export const routes = [homeRoutes]
+
+console.log('route....', routes)
